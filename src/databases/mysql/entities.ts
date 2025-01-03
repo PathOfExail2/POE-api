a2a33d1f3a9a9a2612b228f72c4c4c233b6a4764
김정호 <jeangho293@naver.com>
2025-01-03 16:49:45 +0900

    [feat] reset

--- src/databases/mysql/entities.ts
+++
@@ -1,3 +0,0 @@
-import { User } from '../../modules/users/domain/users.entity';
-
-export default [User];
--- src/libs/ddd/ddd-service.ts
+++
@@ -1,7 +0,0 @@
-import { Inject } from '@nestjs/common';
-import { DddContext } from './ddd-context';
-
-export class DddService {
-  @Inject()
-  private context: DddContext;
-}
