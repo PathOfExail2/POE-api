14800ef44bf439070d0eb67afa493b6e21aef44d
김정호 <jeangho293@naver.com>
2025-02-03 16:11:46 +0900

    [fix] 이렇게하면 안돌아감

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { TransactionInterceptor } from '@libs/interceptors/transaction.interceptor';
 import { GlobalRouterModule } from './modules/global-router.module';
 import { DatabaseModule } from './databases/database.module';
 
 @Module({
   imports: [DatabaseModule, GlobalRouterModule],
   controllers: [],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: TransactionInterceptor,
-    },
-  ],
+  providers: [],
 })
 export class AppModule {}
