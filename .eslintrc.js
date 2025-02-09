f6c3ad1f3d13161c5cd43ce654e7e2aba4393cef
김정호 <jeangho293@naver.com>
2025-02-09 18:38:40 +0900

    [feat] upgrade v11 nest

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['anogle-back'],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'class-methods-use-this': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'no-param-reassign': 'off',
   },
 };
